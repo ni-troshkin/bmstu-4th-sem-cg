34cf1bade842b92404cf2f50d0617ea99bc65528
Nickolay Troshkin <nicktrs@mail.ru>
2021-05-18 21:09:52 +0300

    Comments were added

--- lab_08/python/main.py
+++ lab_08/python/main.py
@@ -7,8 +7,6 @@ from PyQt5.QtGui import QImage, QColor, QPen
 
 from math import sqrt
 
-# комментарии
-
 class Point():
     def __init__(self, x, y):
         self.x = int(x)
@@ -28,9 +26,11 @@ class Vector():
         self.x = x
         self.y = y
     
+    # скалярное произведение
     def product(self, vector):
         return self.x * vector.x + self.y * vector.y
     
+    # векторное произведение
     def cross_product(self, vector):
         return self.x * vector.y - self.y * vector.x
 
@@ -95,7 +95,7 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
             ev.accept()
             x = ev.x()
             y = ev.y()
-
+            # проверка, выбран ли цвет
             try:
                 if self.radio_cutter.isChecked() and self.cutter_box.currentIndex() == 0:
                     raise Warning
@@ -108,10 +108,10 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
             else:
                 self.line_process(x, y, ev)
 
-    # обработка клика на сцену при задании фигуры
+    # обработка клика, нахождение измененных координат при зажатии ctrl или shift
     def line_process(self, x, y, ev = None):
         ctrl = False    # возможность ввода горизонтального/вертикального отрезка
-        shift = False    # возможность привязки одной из координат к отсекателю
+        shift = False    # возможность задания отрезка, параллельного ребру отсекателя
         if ev != None:
             if ev.modifiers() == Qt.ControlModifier:
                 ctrl = True
@@ -135,22 +135,26 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
 
         if shift and len(self.cutter) >= 2 and self.radio_line.isChecked() and self.begin_point != None:
             # находим ребро отсекателя, ближе всего к которому поставлена точка
-            # и привязываем одну из координат к координате этого ребра
-            min_dist = 1500
+            # и достраиваем отрезок с тем же угловым коэффициентом, что и у этого ребра
+            min_dist = 10000    # минимальное расстояние до ребра
             k_min = None
             for i in range(len(self.cutter)):
+                # угловой коэффициент ребра (ах + ву + с = 0)
                 a = (self.cutter[(i + 1) % len(self.cutter)].y - self.cutter[i].y)\
                     / (self.cutter[(i + 1) % len(self.cutter)].x - self.cutter[i].x)
                 b = -1
                 c = self.cutter[i].y - a * self.cutter[i].x
+                # расстояние до i-го ребра отсекателя от первой точки отрезка
                 dist = abs(a * self.begin_point.x + b * self.begin_point.y + c) / sqrt(a * a + b * b)
                 if dist < min_dist:
                     min_dist = dist
                     k_min = a
-            
+            # строится отрезок той же длины, которую задал пользователь, но доворачивается
+            # до нужного углового коэффициента
             length = sqrt((x - self.begin_point.x) ** 2 + (y - self.begin_point.y) ** 2)
             dx = sqrt(length * length / (1 + k_min * k_min))
             dy = dx * k_min
+            # выбор направления построения
             if (x - self.begin_point.x) * dx < 0:
                 dx, dy = -dx, -dy
             
@@ -161,10 +165,11 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
 
     # добавление точки и соединяющей прямой на экран
     def add_to_screen(self, x, y, is_cutter = None):
+        # при построении отрезков
         if self.radio_line.isChecked() and is_cutter == None or is_cutter == False:
-            if self.begin_point == None:
+            if self.begin_point == None:    # первая граница отрезка
                 self.begin_point = Point(x, y)
-            else:
+            else:     # вторая граница отрезка
                 line = Line(self.begin_point, Point(x, y))
                 self.scene.addLine(line.start.x, line.start.y, line.end.x, line.end.y, QPen(self.get_color(self.line_box)))
                 self.lines.append(line)
@@ -172,15 +177,28 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.begin_point = None
             self.draw_point(self.get_color(self.line_box), x, y)
         
+        # при построении отсекателя
         if self.radio_cutter.isChecked() and is_cutter == None or is_cutter == True:
             color = self.get_color(self.cutter_box)
-            self.cutter.append(Point(x, y))
+            # отсекатель замкнут - если до этого был нарисован отсекатель, он стирается
             if self.cutter_is_closed:
                 self.cutter_is_closed = False
+                if self.cutter != []:
+                    self.erase_cutter()
                 self.draw_point(color, x, y)
+                self.cutter.append(Point(x, y))
             else:
+                self.cutter.append(Point(x, y))
                 self.scene.addLine(self.cutter[-2].x, self.cutter[-2].y, 
                                     self.cutter[-1].x, self.cutter[-1].y, QPen(color))
+
+    # стереть отсекатель
+    def erase_cutter(self):
+        for i in range(len(self.cutter) - 1):
+            self.scene.addLine(self.cutter[i].x, self.cutter[i].y, 
+                                    self.cutter[i + 1].x, self.cutter[i + 1].y, QPen(Qt.white))
+        self.scene.addLine(self.cutter[-1].x, self.cutter[-1].y, self.cutter[0].x, self.cutter[0].y, QPen(Qt.white))
+        self.cutter.clear()
 
     # формирование окна с сообщением с заданным текстом
     def show_message(self, title, text, font, icon = QtWidgets.QMessageBox.Warning):
@@ -193,7 +211,7 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
         msg.exec_()
 
-    # добавление точки с точными координатами
+    # добавление точки отрезка с точными координатами
     def add_point(self):
         try:
             x = int(self.x_entry.text())
@@ -208,7 +226,7 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         else:
             self.add_to_screen(x, y, False)
 
-    # добавление точки с точными координатами
+    # добавление вершины отсекателя с точными координатами
     def add_vertex(self):
         try:
             x = int(self.ver_x_entry.text())
@@ -229,10 +247,14 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         self.scene.addLine(x - 1, y, x + 1, y, QPen(color))
         self.scene.addLine(x - 1, y + 1, x + 1, y + 1, QPen(color))
 
+    # проверка выпуклости многоугольника
     def is_convex(self):
+        # меньше трех вершин - какой ты многоугольник после этого?
         if len(self.cutter) < 3:
             return False
         
+        # проверяем, что все векторные произведения смежных сторон отсекателя 
+        # одного знака, за эталон берем первые два ребра
         vect1 = Vector(self.cutter[0], self.cutter[1])
         vect2 = Vector(self.cutter[1], self.cutter[2])
 
@@ -241,15 +263,22 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         else:
             sign = -1
 
-        for i in range(3, len(self.cutter)):
-            vect1 = Vector(self.cutter[i - 2], self.cutter[i - 1])
-            vect2 = Vector(self.cutter[i - 1], self.cutter[i])
-
+        # проверка остальных сторон
+        for i in range(3, len(self.cutter) + 2):
+            vect1 = vect2
+            if i < len(self.cutter):
+                vect2 = Vector(self.cutter[i - 1], self.cutter[i])
+            elif i == len(self.cutter):
+                vect2 = Vector(self.cutter[-1], self.cutter[0])
+            else:
+                vect2 = Vector(self.cutter[0], self.cutter[1])
+            
             if sign * vect1.cross_product(vect2) < 0:
                 return False
 
         return True
     
+    # получение вектора внутренней нормали к ребру
     def get_normal(self, line, check_point):
         vect = Vector(line.start, line.end)
         normal = Vector()
@@ -264,32 +293,39 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         
         return normal
 
+    # отсечение линии выпуклым отсекателем
     def cut_line(self, line):
-        direction = Vector(line.start, line.end)
+        direction = Vector(line.start, line.end)    # вектор директрисы
         t_min = 0
         t_max = 1
         for i in range(len(self.cutter)):
+            # текущее ребро отсекателя
             c_line = Line(self.cutter[i], self.cutter[(i + 1) % len(self.cutter)])
             normal = self.get_normal(c_line, self.cutter[(i + 2) % len(self.cutter)])
+            # гранична точка на ребре
             border_point = self.cutter[i]
+            # вектор от начала отрезка к граничной точке
             w_vect = Vector(border_point, line.start)
-            w_prod = w_vect.product(normal)
-            d_prod = direction.product(normal)
+            
+            w_prod = w_vect.product(normal)     # w * n_i
+            d_prod = direction.product(normal)   # d * n_i
             if d_prod != 0:
+                # отрезок не вырожден в точку и не параллелен стороне отсекателя
                 t = -w_prod / d_prod
-                if d_prod > 0:
+                if d_prod > 0:    # поиск нижней границы t
                     if t > 1:
-                        return
+                        return   # отрезок невидим
                     t_min = max(t, t_min)
-                else:
+                else:    # поиск верхней границы t
                     if t < 0:
                         return
                     t_max = min(t, t_max)
             elif w_prod < 0:
-                return
+                return     # проверка видимости параллельного (или вырожденного) отрезка
         if t_min > t_max:
-            return
+            return     # отрезок невидим
         
+        # визуализация части, оставшейся внутри отсекателя
         color = self.get_color(self.result_box)
         pen = QPen(color)
         pen.setWidth(2)
@@ -302,12 +338,17 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.scene.addLine(start_x, start_y, end_x, end_y, pen)
 
+    # запуск алгоритма отсечения для каждого из отрезков
     def cut(self):
         try:
+            if self.cutter == [] or not self.cutter_is_closed:
+                raise IOError
             if not self.is_convex():
                 raise Warning
             if self.result_box.currentIndex() == 0:
                 raise TypeError
+        except IOError:
+            self.show_message("Ошибка", "Введите замкнутый отсекатель", "Ubuntu 15")
         except Warning:
             self.show_message("Ошибка", "Отсекатель невыпуклый", "Ubuntu 15")
         except:
@@ -319,11 +360,23 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
                 buf_line = Line(start, end)
                 self.cut_line(buf_line)
 
+    # замыкание многоугольника
     def close_cutter(self):
-        color = self.get_color(self.cutter_box)
-        self.scene.addLine(self.cutter[-1].x, self.cutter[-1].y, 
-                                    self.cutter[0].x, self.cutter[0].y, QPen(color))
-        self.cutter_is_closed = True
+        try:
+            if self.cutter_box.currentIndex() == 0:
+                raise Warning
+            if len(self.cutter) < 3:
+                raise TypeError
+        except Warning:
+            self.show_message("Ошибка", "Выберите цвет отсекателя", "Ubuntu 15")
+        except:
+            self.show_message("Ошибка", "Для замкнутого многоугольника необходимо "
+                "хотя бы три точки", "Ubuntu 15")
+        else:
+            color = self.get_color(self.cutter_box)
+            self.scene.addLine(self.cutter[-1].x, self.cutter[-1].y, 
+                                        self.cutter[0].x, self.cutter[0].y, QPen(color))
+            self.cutter_is_closed = True
 
     # очистка экрана
     def clear(self):
