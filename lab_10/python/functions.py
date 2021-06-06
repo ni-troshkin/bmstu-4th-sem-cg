cd6d0731048a315280fb591b76875802b39d4766
Nickolay Troshkin <nicktrs@mail.ru>
2021-06-06 21:14:09 +0300

    Stable version. There are some bugs to fix

--- lab_10/python/functions.py
+++ lab_10/python/functions.py
@@ -4,13 +4,17 @@ def f1(x, z):
     return sin(x) * sin(x) + cos(z) * cos(z)
 
 def f2(x, z):
-    return 2 * cos(x * z)
+    return x**2 / 20 - z**2 / 20
 
 def f3(x, z):
     return exp(sin(sqrt(x**2 + z**2)))
 
+# эллиптический параболоид
 def f4(x, z):
     return x**2 / 20 + z**2 / 20
 
 def f5(x, z):
-    return x*z
+    return x*z/25
+
+def f6(x, z):
+    return sqrt(x**2 + z**2 + 25)
--- lab_10/python/main.py
+++ lab_10/python/main.py
@@ -16,21 +16,19 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         self.clear_btn.clicked.connect(self.clear)
         self.exit_btn.clicked.connect(self.exit)
         self.draw_btn.clicked.connect(self.draw_surface)
-        self.scale_btn.clicked.connect(self.exit)
-        self.x_rotate_btn.clicked.connect(self.exit)
-        self.y_rotate_btn.clicked.connect(self.exit)
-        self.z_rotate_btn.clicked.connect(self.exit)
+        self.scale_btn.clicked.connect(self.scale)
+        self.x_rotate_btn.clicked.connect(self.rotation_x)
+        self.y_rotate_btn.clicked.connect(self.rotation_y)
+        self.z_rotate_btn.clicked.connect(self.rotation_z)
 
         self.width = int(self.scene.width())
         self.height = int(self.scene.height())
-        self.min_horizon = [self.height for _ in range(self.width)]
-        self.max_horizon = [0 for _ in range(self.width)]
-
+        
         self.angle_x = 0
         self.angle_y = 0
         self.angle_z = 0
 
-        self.scale_coeff = 50
+        self.scale_coeff = 1
 
     def show_message(self, title, text, font, icon = QtWidgets.QMessageBox.Warning):
         msg = QtWidgets.QMessageBox()
@@ -76,8 +74,8 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def horizon(self, start_x, start_y, end_x, end_y):
         if end_x == start_x:
-            self.max_horizon[end_x] = max(self.max_horizon[end_x], max(start_y, end_y))
-            self.min_horizon[end_x] = min(self.min_horizon[end_x], min(start_y, end_y))
+            self.max_horizon[end_x] = max(self.max_horizon[end_x], end_y)#max(start_y, end_y))
+            self.min_horizon[end_x] = min(self.min_horizon[end_x], end_y)#min(start_y, end_y))
         else:
             k = (end_y - start_y) / (end_x - start_x)
             for x in range(start_x, end_x + 1):
@@ -87,6 +85,8 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
     
     def is_visible(self, x, y):
         x = int(round(x))
+        if x > self.scene.width():
+            return 0
         if y < self.max_horizon[x] and y > self.min_horizon[x]:
             return 0
         # print("VISIBILITY")
@@ -101,22 +101,22 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
             if a > 0:
                 return 1
             return 0
-        print(x_start, y_start, x_end, y_end)
-        print(x_start, horizon[x_start], x_end, horizon[x_end])
+        # print(x_start, y_start, x_end, y_end)
+        # print(x_start, horizon[x_start], x_end, horizon[x_end])
         if x_end == x_start:
             return x_end, horizon[x_end]
         
         k = (y_end - y_start) / (x_end - x_start)
-        sy = sign(y_start - horizon[x_start])
+        sy = sign(y_start + k - horizon[x_start + 1])
         sc = sy
-        y = y_start
-        x = x_start
-        while sc == sy and x < len(horizon) - 1:
+        y = y_start + k
+        x = x_start + 1
+        while sc == sy and x < x_end:
             sc = sign(y - horizon[x])
             y += k
             x += 1
-        print("INTER")
-        print(int(round(x)), int(round(y)))
+        # print("INTER")
+        # print(int(round(x)), int(round(y)))
         return int(round(x)), int(round(y))
 
     def draw(self, x_from, x_to, x_step, z_from, z_to, z_step, f, color):
@@ -128,7 +128,10 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         x_right = -1
         y_right = -1
 
-        self.scale_coeff = self.width / (x_to - x_from)
+        self.min_horizon = [self.height for _ in range(self.width)]
+        self.max_horizon = [0 for _ in range(self.width)]
+
+        # self.scale_coeff = self.width / (x_to - x_from)
 
         while z >= z_from:
             x_last = x_from
@@ -137,7 +140,9 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
             x_last, y_last, z_buf = self.transform(x_last, y_last, z)
             
             if x_left != -1:
+                self.scene.addLine(x_last, y_last, x_left, y_left, QPen(color))
                 self.horizon(x_last, y_last, x_left, y_left)
+
             x_left = x_last
             y_left = y_last
 
@@ -168,7 +173,7 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
                     elif curr_visibility == 1:
                         if not prev_visibility:
                             x_inter, y_inter = self.intersection(x_last, y_last, x_curr, y_curr, self.max_horizon)
-                            # print("ADDLINE")
+                            # print("ADDLINE") !
                             self.scene.addLine(x_inter, y_inter, x_curr, y_curr, QPen(color))
                             self.horizon(x_inter, y_inter, x_curr, y_curr)
                         else:
@@ -183,7 +188,7 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
                     else:
                         if not prev_visibility:
                             x_inter, y_inter = self.intersection(x_last, y_last, x_curr, y_curr, self.min_horizon)
-                            # print("ADDLINE")
+                            # print("ADDLINE") !
                             self.scene.addLine(x_inter, y_inter, x_curr, y_curr, QPen(color))
                             self.horizon(x_inter, y_inter, x_curr, y_curr)
                         else:
@@ -202,12 +207,11 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
                 x += x_step
 
             if x_right != -1:
+                self.scene.addLine(x_right, y_right, x_curr, y_curr, QPen(color))
                 self.horizon(x_right, y_right, x_curr, y_curr)
             x_right = x_curr
             y_right = y_curr
             z -= z_step
-        print("HERE")
-        self.scene.update()
     
 
     def draw_surface(self):
@@ -238,11 +242,44 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
             self.show_message("Ошибка", "Нижний предел координат должен быть "
                 "меньше/равен верхнего", "Ubuntu 15")
         else:
+            self.scene.clear()
             self.draw(x_from, x_to, x_step, z_from, z_to, z_step, f, color)
-            print("DRAWN")
+
+    def rotation_x(self):
+        try:
+            angle = self.x_rotate_entry.value()
+        except:
+            self.show_message("Ошибка", "Выберите цвет построения", "Ubuntu 15")
+        else:
+            self.angle_x += angle
+            self.draw_surface()
+
+    def rotation_y(self):
+        try:
+            angle = self.y_rotate_entry.value()
+        except:
+            self.show_message("Ошибка", "Выберите цвет построения", "Ubuntu 15")
+        else:
+            self.angle_y += angle
+            self.draw_surface()
+
+    def rotation_z(self):
+        try:
+            angle = self.z_rotate_entry.value()
+        except:
+            self.show_message("Ошибка", "Выберите цвет построения", "Ubuntu 15")
+        else:
+            self.angle_z += angle
+            self.draw_surface()
+    
+    def scale(self):
+        self.scale_coeff = self.scale_entry.value()
+        self.draw_surface()
 
     def clear(self):
         self.scene.clear()
+        self.angle_x = self.angle_y = self.angle_z = 0
+        self.scale_coeff = 1
 
     def exit(self):
         sys.exit(0)
@@ -276,6 +313,8 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
             return f4
         if self.func_box.currentIndex() == 5:
             return f5
+        if self.func_box.currentIndex() == 6:
+            return f6
         return None
 
 if __name__ == '__main__':
