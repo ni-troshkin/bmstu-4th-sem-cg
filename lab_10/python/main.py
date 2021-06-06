a9f88727086536af5ae2172a06a7f9f5284e80ab
Nickolay Troshkin <nicktrs@mail.ru>
2021-06-06 12:16:20 +0300

    Design for lab_10 was added

---
+++ lab_10/python/main.py
@@ -0,0 +1,19 @@
+from app_interface import Ui_MainWindow
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QImage, QColor, QPen
+
+import sys
+from math import sqrt
+
+class Window(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    win = Window()
+    win.show()
+    app.exec()
